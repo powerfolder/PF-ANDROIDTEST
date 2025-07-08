ec1f9e2b99752152b4049acc03142ba83537ba10
PF-bughunter <martin.staude1@web.de>
2025-07-08 18:52:35 +0200

    PFM-648: Update AB1 with new logout module

--- Scripts/AddButton/AB1_VerifyNewPresentation/Script1749708655213.groovy
+++ Scripts/AddButton/AB1_VerifyNewPresentation/Script1749708655213.groovy
@@ -16,70 +16,103 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
+import com.kms.katalon.core.configuration.RunConfiguration as RunConfiguration
 
-import com.kms.katalon.core.configuration.RunConfiguration
-
-if(GlobalVariable.isExistingApp) {
-Mobile.startExistingApplication('de.goddchen.android.powerfolder.A', FailureHandling.STOP_ON_FAILURE)
+if (GlobalVariable.isExistingApp) {
+    Mobile.startExistingApplication('de.goddchen.android.powerfolder.A', FailureHandling.STOP_ON_FAILURE // click on home icon button 
+        )
 } else {
-	String applocation = RunConfiguration.getProjectDir()+'/apks/'+GlobalVariable.AppName;
-	System.out.println("Applocation"+ applocation)
-	Mobile.startApplication(applocation, false, FailureHandling.CONTINUE_ON_FAILURE)
-	Mobile.delay(5)
-	if((Mobile.verifyElementExist(findTestObject('LoginScreen/LoginButton'), 5, FailureHandling.OPTIONAL))) {
-		login()
-	}
-	// click on home icon button 
-	Mobile.tap(findTestObject('LoginScreen/HomeIcon'),30)
-	Mobile.delay(3)}
+    String applocation = (RunConfiguration.getProjectDir() + '/apks/') + GlobalVariable.AppName
+
+    System.out.println('Applocation' + applocation)
+
+    Mobile.startApplication(applocation, false, FailureHandling.CONTINUE_ON_FAILURE)
+
+    Mobile.delay(5)
+
+    if (Mobile.verifyElementExist(findTestObject('LoginScreen/LoginButton'), 5, FailureHandling.OPTIONAL)) {
+        login()
+    }
+    
+    Mobile.tap(findTestObject('LoginScreen/HomeIcon'), 30)
+
+    Mobile.delay(3)
+}
 
 //clicking on 2nd folder
 Mobile.tap(findTestObject('ListContent/Second_folder'), 30)
 
 // Click on plus icon button and select new presentation 
 Mobile.delay(3)
-Mobile.tapAtPosition(GlobalVariable.plusIcontapX , GlobalVariable.plusIcontapY)
+
+Mobile.tapAtPosition(GlobalVariable.plusIcontapX, GlobalVariable.plusIcontapY)
+
 Mobile.delay(3)
-Mobile.verifyElementExist(findTestObject('PlusIconMenus/NewPresentation'),5)
+
+Mobile.verifyElementExist(findTestObject('PlusIconMenus/NewPresentation'), 5)
+
 Mobile.tap(findTestObject('PlusIconMenus/NewPresentation'), 30)
+
 Mobile.delay(3)
 
 //Create and verify new presentation with .pptx extension
-Mobile.verifyElementExist(findTestObject('CreateNewFile/CreateNewFilePopUpHeader'),5)
+Mobile.verifyElementExist(findTestObject('CreateNewFile/CreateNewFilePopUpHeader'), 5)
+
 Mobile.tap(findTestObject('CreateNewFile/CreateNewFileNameField'), 30)
-Mobile.setText(findTestObject('CreateNewFile/CreateNewFileNameField'), "Test Document", 30)
+
+Mobile.setText(findTestObject('CreateNewFile/CreateNewFileNameField'), 'Test Document', 30)
+
 Mobile.delay(1)
-Mobile.tap(findTestObject('CreateNewFile/ClickOnOkButton'),30)
+
+Mobile.tap(findTestObject('CreateNewFile/ClickOnOkButton'), 30)
+
 Mobile.delay(10)
-Mobile.tap(findTestObject('VerifyCreatedFileNames/CloseButton'),30)
+
+Mobile.tap(findTestObject('VerifyCreatedFileNames/CloseButton'), 30)
+
 Mobile.delay(5)
-String getFolderName= Mobile.getText(findTestObject('VerifyCreatedFileNames/VerifyCreatedNewPresentationName'), 30)
+
+String getFolderName = Mobile.getText(findTestObject('VerifyCreatedFileNames/VerifyCreatedNewPresentationName'), 30)
+
 Mobile.verifyEqual(getFolderName, 'Test Document.pptx')
 
 //Swipe to delete created docx.
 //Mobile.swipe(402, 351, 140, 351)
 Mobile.tap(findTestObject('Folder_Menu/Button_Dropdown'), 30)
+
 Mobile.delay(1)
+
 Mobile.tap(findTestObject('SwipeElements/DeleteIcon'), 30)
+
 Mobile.tap(findTestObject('SwipeElements/YesButton'), 30)
+
 Mobile.delay(1)
+
 String alertMsg = Mobile.getText(findTestObject('SwipeElements/DeleteAlertMsg'), 30)
+
 if (alertMsg.contains('Deleted Test Document.pptx')) {
-	println(alertMsg)
-}else {
-	print('text File not deleted')
+    println(alertMsg)
+} else {
+    print('text File not deleted')
 }
 
-//Closing application
-Mobile.closeApplication()
+WebUI.callTestCase(findTestCase('Logout/Logout'), [:], FailureHandling.CONTINUE_ON_FAILURE)
 
 def login() {
-	Mobile.tap(findTestObject('LoginScreen/ServerURL'),30)
-	Mobile.setText(findTestObject('LoginScreen/enterServerURL'), GlobalVariable.ServerURL, 30)
-	Mobile.tap(findTestObject('LoginScreen/ServerURL'),30)
-	Mobile.setText(findTestObject('LoginScreen/EnterEmail'), GlobalVariable.userid, 30)
-	Mobile.setText(findTestObject('LoginScreen/InputPassword'), GlobalVariable.password, 30)
-	Mobile.hideKeyboard()
-	Mobile.tap(findTestObject('LoginScreen/LoginButton'), 45)
-	Mobile.delay(3)
+    Mobile.tap(findTestObject('LoginScreen/ServerURL'), 30)
+
+    Mobile.setText(findTestObject('LoginScreen/enterServerURL'), GlobalVariable.ServerURL, 30)
+
+    Mobile.tap(findTestObject('LoginScreen/ServerURL'), 30)
+
+    Mobile.setText(findTestObject('LoginScreen/EnterEmail'), GlobalVariable.userid, 30)
+
+    Mobile.setText(findTestObject('LoginScreen/InputPassword'), GlobalVariable.password, 30)
+
+    Mobile.hideKeyboard()
+
+    Mobile.tap(findTestObject('LoginScreen/LoginButton'), 45)
+
+    Mobile.delay(3)
 }
+
