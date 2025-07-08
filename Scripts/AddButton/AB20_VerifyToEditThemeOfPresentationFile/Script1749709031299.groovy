3e6a3657bb964f3e4a15df6c9f3b626f0ee0f093
PF-bughunter <martin.staude1@web.de>
2025-07-08 19:24:54 +0200

    PFM-648: Update AB20 with new logout module

--- Scripts/AddButton/AB20_VerifyToEditThemeOfPresentationFile/Script1749709031299.groovy
+++ Scripts/AddButton/AB20_VerifyToEditThemeOfPresentationFile/Script1749709031299.groovy
@@ -85,8 +85,8 @@ if (alertMsg.contains('Deleted Test Document.pptx')) {
 	print('text File not deleted')
 }
 
-// Closing application
-Mobile.closeApplication()
+//logout and close app
+WebUI.callTestCase(findTestCase('Logout/Logout'), [:], FailureHandling.CONTINUE_ON_FAILURE)
 
 def login() {
 	Mobile.tap(findTestObject('LoginScreen/ServerURL'),30)
