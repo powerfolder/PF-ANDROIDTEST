60fe06a4e6ee8dace1effce335d895554cb1605f
PF-bughunter <martin.staude1@web.de>
2025-07-08 19:12:32 +0200

    PFM-648: Update AB5 with new logout module

--- Scripts/AddButton/AB5_VerifyToCreateDocumentWithoutFileName/Script1749708917630.groovy
+++ Scripts/AddButton/AB5_VerifyToCreateDocumentWithoutFileName/Script1749708917630.groovy
@@ -53,8 +53,8 @@ if(GlobalVariable.isExistingApp) {
 	Mobile.pressBack()
 	Mobile.delay(2)
 	
-	//Closing application
-	Mobile.closeApplication()
+	//logout and close app
+    WebUI.callTestCase(findTestCase('Logout/Logout'), [:], FailureHandling.CONTINUE_ON_FAILURE)
 	
 	def login() {
 	Mobile.tap(findTestObject('LoginScreen/ServerURL'),30)
