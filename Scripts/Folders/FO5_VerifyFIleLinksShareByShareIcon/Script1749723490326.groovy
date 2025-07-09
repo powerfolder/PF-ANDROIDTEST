f3033a879585f526107aff68cf605ed31ce8261a
PF-bughunter <martin.staude1@web.de>
2025-07-09 17:59:33 +0200

    PFM-648: Update FO5 with new logout module

--- Scripts/Folders/FO5_VerifyFIleLinksShareByShareIcon/Script1749723490326.groovy
+++ Scripts/Folders/FO5_VerifyFIleLinksShareByShareIcon/Script1749723490326.groovy
@@ -78,7 +78,8 @@ if (alertMsg.contains('Deleted')) {
 }else {
 	print('File not deleted')
 }
-Mobile.closeApplication()
+//logout and close app
+WebUI.callTestCase(findTestCase('Logout/Logout'), [:], FailureHandling.CONTINUE_ON_FAILURE)
 
 def login() {
 	Mobile.tap(findTestObject('LoginScreen/ServerURL'),30)
