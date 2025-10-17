cada6949d2728201d0685333d8b9c531ec272be8
PF-bughunter <martin.staude1@web.de>
2025-10-17 22:21:34 +0200

    PFM-665: update AB02-Ab22 to use globalvar to notice change of app version

--- Scripts/AddButton/AB12_VerifyToCreateSubDirectoryWithoutFileName/Script1749708969279.groovy
+++ Scripts/AddButton/AB12_VerifyToCreateSubDirectoryWithoutFileName/Script1749708969279.groovy
@@ -21,7 +21,7 @@ import com.kms.katalon.core.testobject.ConditionType
 import com.kms.katalon.core.testobject.TestObject
 
 // start up app
-CustomKeywords.'utils.Startup_app.install'('PowerFolder_v23.1.101.apk')
+CustomKeywords.'utils.Startup_app.install'(GlobalVariable.AppName)
 
 // proceed login not logged in
 if (Mobile.verifyElementExist(findTestObject('LoginScreen/LoginButton'), 5, FailureHandling.OPTIONAL)) {
