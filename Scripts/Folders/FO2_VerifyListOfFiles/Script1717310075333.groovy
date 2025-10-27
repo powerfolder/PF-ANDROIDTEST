1c1fdd9d7d976310e82d3bab3fdbea425427ad03
PF-bughunter <martin.staude1@web.de>
2025-10-27 18:06:44 +0100

    PFM-664: update testcase FO2

--- Scripts/Folders/FO2_VerifyListOfFiles/Script1717310075333.groovy
+++ Scripts/Folders/FO2_VerifyListOfFiles/Script1717310075333.groovy
@@ -16,44 +16,145 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
+import org.openqa.selenium.Keys as Keys
+import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.configuration.RunConfiguration as RunConfiguration
 
-import com.kms.katalon.core.configuration.RunConfiguration
-import com.kms.katalon.core.configuration.RunConfiguration
+// start up app
+//CustomKeywords.'utils.Startup_app.install'()
+CustomKeywords.'utils.Startup_app.install'(GlobalVariable.AppName)
 
+// proceed login not logged in
+if (Mobile.verifyElementExist(findTestObject('LoginScreen/LoginButton'), 5, FailureHandling.OPTIONAL)) {
+	CustomKeywords.'utils.Process_login.login'(GlobalVariable.ServerURL,GlobalVariable.userid, GlobalVariable.password)
+}
 
-if(GlobalVariable.isExistingApp) {
-Mobile.startExistingApplication('de.goddchen.android.powerfolder.A', FailureHandling.STOP_ON_FAILURE)
-} else {
-	String applocation = RunConfiguration.getProjectDir()+'/apks/'+GlobalVariable.AppName;
-	System.out.println("Applocation"+ applocation)
-	Mobile.startApplication(applocation, false, FailureHandling.CONTINUE_ON_FAILURE)
-	Mobile.delay(5)
-	if((Mobile.verifyElementExist(findTestObject('LoginScreen/LoginButton'), 5, FailureHandling.OPTIONAL))) {
-		login()
-	}
-	// click on home icon button 
-	Mobile.tap(findTestObject('LoginScreen/HomeIcon'),30)
-	Mobile.delay(3)}
- 
-// 
-String List1 = Mobile.getText(findTestObject('ListContent/Content1'), 30)
-Mobile.verifyEqual(List1, 'Base')
-Mobile.delay(1)
-String List2 = Mobile.getText(findTestObject('ListContent/Content2'), 30)
-//Mobile.verifyEqual(List1, 'Mobile Uploads')
+// tab on fab_button - plus-button
+Mobile.delay(3)
+Mobile.tapAtPosition(GlobalVariable.EMU_P8_plusIconTabX, GlobalVariable.EMU_P8_plusIconTabY)
+Mobile.delay(3)
+
+// tab on menu-entry New-Directory to start Toplvl-folder-creation dialog
+Mobile.tap(findTestObject('PlusIconMenus/NewDirectory'), 30)
+
+// create foldername based on timestamp
+String timestamp_folder = CustomKeywords.'utils.Get_timestamp.generateTimestamp'()
+String folderName = 'Folder_' + timestamp_folder
+
+Mobile.setText(findTestObject('Folder_Menu/EnterNewFolderName'), folderName, 30)
 Mobile.delay(2)
+Mobile.tap(findTestObject('Folder_Menu/ClickOnOkButton'), 30)
+
+// wait some seconds after setting up new toplvl folder
+Mobile.delay(3)
+
+// verifying folder is existing
+TestObject top_folder_obj = new TestObject()
+top_folder_obj.addProperty("xpath", ConditionType.EQUALS, "//*[@text='" + folderName + "']")
+Mobile.verifyElementExist(top_folder_obj, 5)
+
+// tab on toplvl folder
+Mobile.delay(2)
+Mobile.tap(top_folder_obj, 5)
+
+// Get the current timestamp for unique naming
+String timestamp_file = CustomKeywords.'utils.Get_timestamp.generateTimestamp'()
+
+// Generate names for different files
+String presentationName = 'Presentation_' + timestamp_file
+String docxName = 'Document_' + timestamp_file
+String textFileName = 'TextFile_' + timestamp_file
+String spreadsheetName = 'Spreadsheet_' + timestamp_file
+
+// Open Plus Icon
+Mobile.delay(4)
+Mobile.tapAtPosition(GlobalVariable.EMU_P8_plusIconTabX, GlobalVariable.EMU_P8_plusIconTabY)
+Mobile.delay(6)
+
+// Create Presentation File
+Mobile.tap(findTestObject('PlusIconMenus/NewPresentation'), 30)
+Mobile.verifyElementExist(findTestObject('CreateNewFile/CreateNewFilePopUpHeader'), 10)
+Mobile.delay(2)
+Mobile.tap(findTestObject('CreateNewFile/CreateNewFileNameField'), 30)
+Mobile.setText(findTestObject('CreateNewFile/CreateNewFileNameField'), presentationName, 30)
+Mobile.tap(findTestObject('CreateNewFile/ClickOnOkButton'), 30)
+Mobile.delay(10)
+Mobile.tap(findTestObject('VerifyCreatedFileNames/CloseButton'), 30)
+Mobile.delay(5)
+
+// Verify Presentation file exists
+TestObject presentation_obj = new TestObject()
+presentation_obj.addProperty("xpath", ConditionType.EQUALS, "//*[@text='" + presentationName + ".pptx']")
+Mobile.verifyElementExist(presentation_obj, 5)
+
+// Reopen Plus Icon for the next file type
+Mobile.tapAtPosition(GlobalVariable.EMU_P8_plusIconTabX, GlobalVariable.EMU_P8_plusIconTabY)
+Mobile.delay(6)
+
+// Create Word Document (.docx) File
+Mobile.tap(findTestObject('PlusIconMenus/NewDocument'), 30)
+Mobile.verifyElementExist(findTestObject('CreateNewFile/CreateNewFilePopUpHeader'), 10)
+Mobile.delay(2)
+Mobile.tap(findTestObject('CreateNewFile/CreateNewFileNameField'), 30)
+Mobile.setText(findTestObject('CreateNewFile/CreateNewFileNameField'), docxName, 30)
+Mobile.tap(findTestObject('CreateNewFile/ClickOnOkButton'), 30)
+Mobile.delay(10)
+Mobile.tap(findTestObject('VerifyCreatedFileNames/CloseButton'), 30)
+Mobile.delay(5)
+
+// Verify Document file exists
+TestObject docx_obj = new TestObject()
+docx_obj.addProperty("xpath", ConditionType.EQUALS, "//*[@text='" + docxName + ".docx']")
+Mobile.verifyElementExist(docx_obj, 5)
+
+// Reopen Plus Icon for the next file type
+Mobile.tapAtPosition(GlobalVariable.EMU_P8_plusIconTabX, GlobalVariable.EMU_P8_plusIconTabY)
+Mobile.delay(6)
+
+// Create Text File (.txt)
+Mobile.tap(findTestObject('PlusIconMenus/NewTextFile'), 30)
+Mobile.verifyElementExist(findTestObject('CreateNewFile/CreateNewFilePopUpHeader'), 10)
+Mobile.delay(2)
+Mobile.tap(findTestObject('CreateNewFile/CreateNewFileNameField'), 30)
+Mobile.setText(findTestObject('CreateNewFile/CreateNewFileNameField'), textFileName, 30)
+Mobile.tap(findTestObject('CreateNewFile/ClickOnOkButton'), 30)
+Mobile.delay(10)
+Mobile.tap(findTestObject('VerifyCreatedFileNames/CloseButton'), 30)
+Mobile.delay(5)
+
+// Verify Text file exists
+TestObject textFile_obj = new TestObject()
+textFile_obj.addProperty("xpath", ConditionType.EQUALS, "//*[@text='" + textFileName + ".txt']")
+Mobile.verifyElementExist(textFile_obj, 5)
+
+// Reopen Plus Icon for the next file type
+Mobile.tapAtPosition(GlobalVariable.EMU_P8_plusIconTabX, GlobalVariable.EMU_P8_plusIconTabY)
+Mobile.delay(4)
+
+// Create Spreadsheet (.xlsx) File
+Mobile.tap(findTestObject('PlusIconMenus/NewSpreadsSheet'), 30)
+Mobile.verifyElementExist(findTestObject('CreateNewFile/CreateNewFilePopUpHeader'), 10)
+Mobile.delay(2)
+Mobile.tap(findTestObject('CreateNewFile/CreateNewFileNameField'), 30)
+Mobile.setText(findTestObject('CreateNewFile/CreateNewFileNameField'), spreadsheetName, 30)
+Mobile.tap(findTestObject('CreateNewFile/ClickOnOkButton'), 30)
+Mobile.delay(10)
+Mobile.tap(findTestObject('VerifyCreatedFileNames/CloseButton'), 30)
+Mobile.delay(5)
+
+// Verify Spreadsheet file exists
+TestObject spreadsheet_obj = new TestObject()
+spreadsheet_obj.addProperty("xpath", ConditionType.EQUALS, "//*[@text='" + spreadsheetName + ".xlsx']")
+Mobile.verifyElementExist(spreadsheet_obj, 5)
+
+// go to home - toplvl
+Mobile.tap(findTestObject('LoginScreen/HomeIcon'),30)
+Mobile.delay(2)
+
+// delete created toplvl-folder with presentation inside
+CustomKeywords.'utils.Delete_object.swipeAndDelete'(top_folder_obj)
 
 //logout and close app
 WebUI.callTestCase(findTestCase('Logout/Logout'), [:], FailureHandling.CONTINUE_ON_FAILURE)
-
-
-def login() {
-	Mobile.tap(findTestObject('LoginScreen/ServerURL'),30)
-	Mobile.setText(findTestObject('LoginScreen/enterServerURL'), GlobalVariable.ServerURL, 30)
-	Mobile.tap(findTestObject('LoginScreen/ServerURL'),30)
-	Mobile.setText(findTestObject('LoginScreen/EnterEmail'), GlobalVariable.userid, 30)
-	Mobile.setText(findTestObject('LoginScreen/InputPassword'), GlobalVariable.password, 30)
-	Mobile.hideKeyboard()
-	Mobile.tap(findTestObject('LoginScreen/LoginButton'), 45)
-	Mobile.delay(3)
-}
