876e9bb4240760d4914636052908b2500e00a0a7
PF-bughunter <martin.staude1@web.de>
2026-01-02 12:50:07 +0100

    PFM-721: update A15 rename files testcases to use correct logout method

--- Scripts/Android 15/TestForRenameFilesAndFolders/RF9_VerifyToRenameTextFile/Script1749709251375.groovy
+++ Scripts/Android 15/TestForRenameFilesAndFolders/RF9_VerifyToRenameTextFile/Script1749709251375.groovy
@@ -110,4 +110,4 @@ Mobile.delay(3)
 CustomKeywords.'utils.Delete_object.swipeAndDelete'(top_folder_obj)
 
 //logout and close app
-WebUI.callTestCase(findTestCase('Android 16/Logout/Logout'), [:], FailureHandling.CONTINUE_ON_FAILURE)
+WebUI.callTestCase(findTestCase('Android 15/Logout/Logout'), [:], FailureHandling.CONTINUE_ON_FAILURE)
