85e59d4652f5b529256dffdb248c54605d55f28f
PF-bughunter <martin.staude1@web.de>
2026-07-07 14:34:18 +0200

    PFM-787: add testcases for move-operation

---
+++ Object Repository/SwipeMove/MoveIcon.rs
@@ -0,0 +1,63 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<MobileElementEntity>
+   <description></description>
+   <name>MoveIcon</name>
+   <tag></tag>
+   <elementGuidId>3c106c99-d594-42d0-b197-50b7164ad2ab</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <locator>//*[@class = 'android.widget.TextView' and (@text = 'drive_file_move' or . = 'drive_file_move')]</locator>
+   <locatorCollection>
+      <entry>
+         <key>ANDROID_VIEWTAG</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>IOS_CLASS_CHAIN</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>ACCESSIBILITY</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>ATTRIBUTES</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>ID</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>ANDROID_UI_AUTOMATOR</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>CLASS_NAME</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>NAME</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@class = 'android.widget.TextView' and (@text = 'drive_file_move' or . = 'drive_file_move')]</value>
+      </entry>
+      <entry>
+         <key>IOS_PREDICATE_STRING</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>IMAGE</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>CUSTOM</key>
+         <value></value>
+      </entry>
+   </locatorCollection>
+   <locatorStrategy>XPATH</locatorStrategy>
+   <platform>ANDROID</platform>
+</MobileElementEntity>
---
+++ Scripts/Android 14/VerifyMove/VM5_MoveSubSubFolder_ToSubFolder_SameTopLevelFolder/Script1782207553376.groovy
@@ -0,0 +1,169 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory
+import io.appium.java_client.AppiumDriver
+import io.appium.java_client.remote.SupportsContextSwitching;
+import io.appium.java_client.android.AndroidDriver
+import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.configuration.RunConfiguration as RunConfiguration
+
+// get info about qa-system
+CustomKeywords.'utils.Startup_app.loadCredsIntoGlobals'("katalon.txt")
+
+// start up app
+CustomKeywords.'utils.Startup_app.install'(GlobalVariable.AppName)
+
+// proceed login not logged in
+if (Mobile.verifyElementExist(findTestObject('LoginScreen/LoginButton'), 5, FailureHandling.OPTIONAL)) {
+	CustomKeywords.'utils.Process_login.login'(GlobalVariable.ServerURL, GlobalVariable.userid, GlobalVariable.password)
+}
+
+// tap on fab_button - plus-button
+Mobile.delay(3)
+Mobile.tapAtPosition(GlobalVariable.EMU_P8_plusIconTabX, GlobalVariable.EMU_P8_plusIconTabY)
+Mobile.delay(3)
+
+// tap on menu-entry New-Directory to start Toplvl-folder-creation dialog
+Mobile.tap(findTestObject('PlusIconMenus/NewDirectory'), 30)
+
+// create foldername based on timestamp
+String timestamp_folder = CustomKeywords.'utils.Get_timestamp.generateTimestamp'()
+String folderName = 'Folder_' + timestamp_folder
+
+Mobile.setText(findTestObject('Folder_Menu/EnterNewFolderName'), folderName, 30)
+Mobile.delay(2)
+Mobile.tap(findTestObject('Folder_Menu/ClickOnOkButton'), 30)
+
+// wait some seconds after setting up new toplvl folder
+Mobile.delay(3)
+
+// verifying folder is existing
+TestObject top_folder_obj = new TestObject()
+top_folder_obj.addProperty("xpath", ConditionType.EQUALS, "//*[@text='" + folderName + "']")
+Mobile.verifyElementExist(top_folder_obj, 5)
+
+// tap on first toplvl folder
+Mobile.delay(2)
+Mobile.tap(top_folder_obj, 5)
+
+// tap on fab_button - plus-button
+Mobile.delay(5)
+Mobile.tapAtPosition(GlobalVariable.EMU_P8_plusIconTabX, GlobalVariable.EMU_P8_plusIconTabY)
+Mobile.delay(3)
+
+// tap on menu-entry New-Directory to start Toplvl-folder-creation dialog
+Mobile.tap(findTestObject('PlusIconMenus/NewDirectory'), 30)
+
+// create second folder name based on timestamp
+String secSubFolderName = 'SecSubFolder_' + timestamp_folder
+
+Mobile.setText(findTestObject('Folder_Menu/EnterNewFolderName'),secSubFolderName, 30)
+Mobile.delay(2)
+Mobile.tap(findTestObject('Folder_Menu/ClickOnOkButton'), 30)
+
+// wait some seconds after setting up second sub folder
+Mobile.delay(3)
+
+// verifying second sub folder is existing
+TestObject second_sub_folder_obj = new TestObject()
+second_sub_folder_obj.addProperty("xpath", ConditionType.EQUALS, "//*[@text='" + secSubFolderName + "']")
+Mobile.verifyElementExist(second_sub_folder_obj, 5)
+
+
+// tap on fab_button - plus-button
+Mobile.delay(5)
+Mobile.tapAtPosition(GlobalVariable.EMU_P8_plusIconTabX, GlobalVariable.EMU_P8_plusIconTabY)
+Mobile.delay(3)
+
+// tap on menu-entry New-Directory to start Toplvl-folder-creation dialog
+Mobile.tap(findTestObject('PlusIconMenus/NewDirectory'), 30)
+
+// create sub folder name based on timestamp
+String subFolderName = 'SubFolder_' + timestamp_folder
+
+Mobile.setText(findTestObject('Folder_Menu/EnterNewFolderName'), subFolderName, 30)
+Mobile.delay(2)
+Mobile.tap(findTestObject('Folder_Menu/ClickOnOkButton'), 30)
+
+// wait some seconds after setting up sub folder
+Mobile.delay(3)
+
+// verifying sub folder is existing
+TestObject sub_folder_obj = new TestObject()
+sub_folder_obj.addProperty("xpath", ConditionType.EQUALS, "//*[@text='" + subFolderName + "']")
+Mobile.verifyElementExist(sub_folder_obj, 5)
+
+// tap on first toplvl folder
+Mobile.delay(2)
+Mobile.tap(sub_folder_obj, 5)
+
+
+// tap on fab_button - plus-button
+Mobile.delay(5)
+Mobile.tapAtPosition(GlobalVariable.EMU_P8_plusIconTabX, GlobalVariable.EMU_P8_plusIconTabY)
+Mobile.delay(3)
+
+// tap on menu-entry New-Directory to start Toplvl-folder-creation dialog
+Mobile.tap(findTestObject('PlusIconMenus/NewDirectory'), 30)
+
+// create subsub folder name based on timestamp
+String subsubFolderName = 'subSubFolder_' + timestamp_folder
+Mobile.setText(findTestObject('Folder_Menu/EnterNewFolderName'), subsubFolderName, 30)
+Mobile.delay(2)
+Mobile.tap(findTestObject('Folder_Menu/ClickOnOkButton'), 30)
+
+// wait some seconds after setting up subsub folder
+Mobile.delay(3)
+
+// verifying subsub folder is existing
+TestObject subsub_folder_obj = new TestObject()
+subsub_folder_obj.addProperty("xpath", ConditionType.EQUALS, "//*[@text='" + subsubFolderName + "']")
+Mobile.verifyElementExist(subsub_folder_obj, 5)
+
+// Swap left to right for move target folder
+CustomKeywords.'utils.SwipeLeftToRight.swipe'(subsub_folder_obj)
+
+// Tap on move icon
+Mobile.tap(findTestObject('SwipeMove/MoveIcon'), 30)
+
+// Press back
+Mobile.pressBack()
+
+// Tap to open second sub folder
+Mobile.tap(second_sub_folder_obj, 5)
+
+// Tap on move button
+Mobile.tap(findTestObject('SwipeMove/MoveButton'), 30)
+Mobile.delay(5)
+
+// Verify subsub folder after move on target folder
+Mobile.verifyElementExist(subsub_folder_obj, 5)
+
+// go to home - toplvl
+Mobile.tap(findTestObject('LoginScreen/HomeIcon'), 30)
+Mobile.delay(2)
+
+// delete created toplvl-folder with presentation inside
+CustomKeywords.'utils.Delete_object.swipeAndDelete'(top_folder_obj)
+
+// logout and close app
+WebUI.callTestCase(findTestCase('Android 14/Logout/Logout'), [:], FailureHandling.CONTINUE_ON_FAILURE)
